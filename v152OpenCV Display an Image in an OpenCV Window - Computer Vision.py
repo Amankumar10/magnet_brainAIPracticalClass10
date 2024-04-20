79f5b8907c4ab514ff461a1cfc63917d82578329
Amankumar10 <amankumar02740@gmail.com>
2024-04-20 17:10:50 +0530

    v152 51:04

--- v152OpenCV Display an Image in an OpenCV Window - Computer Vision.py
+++ v152OpenCV Display an Image in an OpenCV Window - Computer Vision.py
@@ -9,9 +9,24 @@ print(type(img))
 #Display
 
 cv2.imshow('Our image', img)
+# var =cv2.imshow('Our image', img)
+# print(var)
 
+#Wait for key press to close window
+# cv2.waitKey(0)
 #Display for longer duration or To copy keyboard input 
 # cv2.waitKey(5)
-x=cv2.waitKey(0)
-print('Key pressed-->',x)
+# x=cv2.waitKey(0)
+# print('Key pressed-->',x)
 
+
+# Destroy
+# cv2.destroyAllWindows()
+
+
+#Different flags
+img= cv2.imread('./data/Screenshot from 2024-04-20 16-35-03.png',0)
+cv2.imshow('Our image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# cv2.destroyAllWindows('./data/Screenshot from 2024-04-20 16-35-03.png')
